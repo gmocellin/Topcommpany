36db7dccb6c678c87f1862415d40312ce981054e
gilzoide <gilzoide@gmail.com>
2015-11-19 16:24:02 -0200

    Twisted, erro de conexão
    UPDATE: servidor e cliente agora usam o Twisted para conexão (UDP)
    
    UPDATE: servidor ouve UDP e echoa, suporta múltiplas conexões
    
    NEW: quando a conexão não é possível, app tem uma tela de erro

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 #coding: utf-8
 
+# Twisted pra rede funcionar
+# Função install_twisted_reactor deve ser importado antes do Twisted
+from kivy.support import install_twisted_reactor
+install_twisted_reactor()
+
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+class SensorSender (DatagramProtocol):
+    def __init__ (self, app, name, host, port):
+        self.app = app
+        self.name = name
+        self.host = host
+        self.port = port
+
+    def startProtocol (self):
+        self.transport.connect (self.host, self.port)
+        Clock.schedule_interval (self.sendSensors, 1)
+
+    def stopProtocol (self):
+        Clock.unschedule (self.sendSensors)
+
+    def sendSensors (self, dt):
+        self.transport.write (self.name)
+
+    def connectionRefused (self):
+        self.app.erroConexao ()
+
+# Kivy, nossa interface gráfica
+
 import kivy
 
 from kivy.app import App
@@ -7,11 +37,7 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
 
-import socket
 from random import randint
-
-# socket cliente global
-sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
 
 # carrega o arquivo kivy das telas
 Builder.load_file ('telas.kv')
@@ -22,37 +48,38 @@ class TelaConexao (Screen):
 
 class TelaEnvio (Screen):
     """Tela de leitura do sensor e envio para servidor"""
-    def schedule (self):
-        Clock.schedule_interval (self.sendInfo, 1)
+    pass
 
-    def unschedule (self):
-        Clock.unschedule (self.sendInfo)
-
-    def sendInfo (self, dt):
-        sensor = str (randint (20, 25))
-        self.children[0].children[1].text = sensor + ' graus'
-        sock.sendall (sensor)
-
+class TelaErroConexao (Screen):
+    """Tela que mostra que a conexão falhou"""
+    pass
 
 class MyApp(App):
     """DataSystemTruck Kivy App"""
+
+    connection = None
+
     def connect (self, ip, port):
         # tenta conectar, retornando se funcionou
-        try:
-            sock.connect ((ip, int (port)))
-            print 'conectei'
-            return True
-        except:
-            sock.close ()
-            return False
+        self.connection = SensorSender (self, 'oi', ip, int (port))
+        reactor.listenUDP (0, self.connection)
+        print 'conectado'
 
     def disconnect (self):
-        sock.close ()
+        if self.connection:
+            self.connection.transport.stopListening ()
+            self.connection = None
 
-    def build(self):
+    def erroConexao (self):
+        self.disconnect ()
+        self.sm.current = 'telaErroConexao'
+
+    def build (self):
         sm = ScreenManager ()
         sm.add_widget (TelaConexao (name = 'telaConexao'))
         sm.add_widget (TelaEnvio (name = 'telaEnvio'))
+        sm.add_widget (TelaErroConexao (name = 'telaErroConexao'))
+        self.sm = sm
         return sm
 
 
--- server.py
+++ server.py
@@ -1,25 +1,12 @@
 #coding: utf-8
 
-import socket
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
 
-endereco = ('localhost', 10000)
-sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+class Echo (DatagramProtocol):
+    def datagramReceived (self, data, (host, port)):
+        print "received %r from %s:%d" % (data, host, port)
+        #self.transport.write (data, (host, port))
 
-sock.bind (endereco)
-sock.listen (1)
-while True:
-    print 'Esperando conexão...'
-    connection, client_address = sock.accept()
-    print 'Conexão por', client_address
-    try:
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(24)
-            print 'Sensor indica: %s graus' % data
-            if not data:
-                print 'Fim da leitura do', client_address
-                break
-
-    finally:
-        # Clean up the connection
-        connection.close()
+reactor.listenMulticast (10000, Echo (), listenMultiple = True)
+reactor.run ()
